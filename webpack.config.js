45e7b08ab283d54e9ba78a3ce40004d515aa5df4
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-08-13 10:06:45 -0600

    More formating

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import {dirname, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
 const dirPath = dirname(fileURLToPath(import.meta.url));
 export default {
 	entry: resolve(dirPath, './index.js'),
 	output: {
-		path: resolve(dirPath, "commonjs"),
-		filename: "bundle.cjs"
+		path: resolve(dirPath, 'commonjs'),
+		filename: 'bundle.cjs',
 	},
 	experiments: {
 		outputModule: true,
 	}
-}
+};

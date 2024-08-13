e0cde1c8b3dd16c335e3a31fd102d3f9090353cc
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-08-13 09:59:51 -0600

    update formating and push properly

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 const dirPath = dirname(fileURLToPath(import.meta.url));
 export default {
-    entry: resolve(dirPath, './index.js'),
-    output: {
-        path: resolve(dirPath, "commonjs"),
-        filename: "bundle.cjs"
-    },
-    experiments: {
-        outputModule: true,
-    }
+	entry: resolve(dirPath, './index.js'),
+	output: {
+		path: resolve(dirPath, "commonjs"),
+		filename: "bundle.cjs"
+	},
+	experiments: {
+		outputModule: true,
+	}
 }
